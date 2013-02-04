3b29dd60738133d8a917637e42c4978baa893b8b
root <root@localhost.localdomain>
2013-02-03 23:08:55 -0500

    Changed about us email to write to database instead

--- processEmail.php
+++ processEmail.php
@@ -1,11 +1,19 @@
 <?php
-  $emailTo = 'shindigssite@gmail.com';
+  include 'dbConnection.php';
+
+  $id = time();
+
+  $query = 'INSERT INTO email (id, message, name, address) VALUES (' . $id . ', \'' . str_replace("'", "\\'", $_POST['Message']) . '\', \'' . str_replace("'", "\\'", $_POST['Name']) . '\', \'' . str_replace("'", "\\'", $_POST['Email']) . '\')';
+
+  mysql_query($query);
+  mysql_close($con);
+  header ("location:aboutUs.php");
+
+  /*$emailTo = 'shindigssite@gmail.com';
   $subjectLine = 'Message From ' . $_POST['Name'];
   $messageBody = 'Name: ' . $_POST['Name'] 
 		  . '\nEmail: ' . $_POST['Email']
 		  . '\nMessage: ' . $_POST['Message'];
   
-  mail($emailTo, $subjectLine, $messageBody);
-
-  header ("location:aboutUs.php");
+  mail($emailTo, $subjectLine, $messageBody);*/
 ?>
